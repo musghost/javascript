ff199ada1d98cda7d2b54eefd9f36ed39b6354f9
Arturo Andrés Cidel Ortega <andres@cidel.com.mx>
2017-02-17 11:07:27 -0600

    Declarar arreglos, agregar y quitar elementos

---
+++ declarar-arreglos-quitar-y-poner.js
@@ -0,0 +1,26 @@
+// Arreglos
+
+// Declarar un arreglo
+
+arreglo = new Array();
+arreglo = new Array(2); // arreglo de dos posiciones
+arreglo = []; //Usar esta
+arreglo = ['Hola', 'mundo', 'adios'];
+
+
+// Definir elementos de arreglo
+arreglo[0] = 'Otra cosa';
+arreglo[20] = 'Otra cosa';
+
+// Agregar elemento al final del arreglo
+arreglo.push('algodon');
+
+// Quitar elementos de en medio
+arreglo.unshift('algo');//Esto es como push pero agrega al principio
+
+// Quitar último elemento
+arreglo.pop();//regresa el elemento que quitamos
+
+// Quitar el primer elemento
+arreglo.shift();//regresa el elemento que quitamos
+
